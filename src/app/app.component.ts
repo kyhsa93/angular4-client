ede781e357f407a51b97db3ce1757d1975013c65
kyhsa93 <kyhsa93@naver.com>
2017-10-09 16:28:41 +0900

    Add http GET test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  private title = 'app';
+  private getData: number;
+  private getResultId: any;
+  private getResultName: any;
+  private getResultSeq: any;
+  
+  constructor(private http: Http ) {
+      
+  }
+
+  clicked() {
+    this.http.get('http://localhost:5000/get-test/' + this.getData)
+    .map(response => {
+      return response.json();
+    })
+    .subscribe(data => {
+        this.getResultId = data[0].id;
+        this.getResultName = data[0].name;
+        this.getResultSeq = data[0].seq;
+    }, error => {
+      console.log(error)
+    });
+  }
 }
