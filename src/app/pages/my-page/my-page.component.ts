c828821fcaf3b09f4b25023bbb402204e924db3d
kyhsa93 <kyhsa93@naver.com>
2017-10-31 20:53:10 +0900

    my page 기능 추가

--- src/app/pages/my-page/my-page.component.ts
+++ src/app/pages/my-page/my-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertsService } from 'angular-alert-module';
+import { Http } from '@angular/http';
 
 @Component({
     templateUrl: './my-page.component.html',
@@ -8,9 +9,16 @@ import { AlertsService } from 'angular-alert-module';
 })
 
 export class MyPageComponent implements OnInit{
+
+    private id: string = '';
+    private password: string = '';
+    private name: string = '';
+    private date: string = '';
+
     constructor (
         private router: Router,
-        private alerts:AlertsService
+        private alerts: AlertsService,
+        private http: Http
     ) {}
 
     /**
@@ -20,6 +28,30 @@ export class MyPageComponent implements OnInit{
         if (!sessionStorage.getItem('id')) {
             this.alerts.setMessage('Please Login', 'error');
             this.router.navigate(['/login']);
+            return;
         }
+        this.http.get('http://localhost:5000/my-page-data/' + sessionStorage.getItem('id')).
+            subscribe(data => {
+                this.id = data.json()[0].id;
+                this.password = data.json()[0].password;
+                this.name = data.json()[0].name;
+                this.date = data.json()[0].sign_up_date;
+            }, error => {
+                this.alerts.setMessage('Fail to load data', 'error');
+            })
+    }
+
+    submit () {
+        let modifiedData = [
+            this.password,
+            this.id
+        ];
+
+        this.http.post('http://localhost:5000/update-sign-up-data', modifiedData).
+            subscribe(response => {
+                this.alerts.setMessage('Submit success', 'success');
+            }, error => {
+                this.alerts.setMessage('Submit fail', 'error')
+            });
     }
 };
