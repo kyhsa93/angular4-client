bc20557821a00a1b88faa70ca80365dd6ec4ec64
kyhsa93 <kyhsa93@naver.com>
2017-11-17 15:48:50 +0900

    전화번호 및 password 변경 기능 수정

--- src/app/pages/my-page/my-page.component.ts
+++ src/app/pages/my-page/my-page.component.ts
@@ -15,7 +15,7 @@ export class MyPageComponent implements OnInit{
     private phone: string = '';
     private date: string = '';
     private confirmed: boolean = false;
-    private saveError: boolean = false;
+    private errorMessage: string = '';
 
     constructor (
         private router: Router,
@@ -33,7 +33,6 @@ export class MyPageComponent implements OnInit{
         this.http.get('http://localhost:5000/my-page-data/' + sessionStorage.getItem('id')).
             subscribe(data => {
                 this.id = data.json()[0].id;
-                this.password = data.json()[0].password;
                 this.name = data.json()[0].name;
                 this.phone = data.json()[0].phone;
                 this.date = data.json()[0].sign_up_date;
@@ -43,16 +42,26 @@ export class MyPageComponent implements OnInit{
     }
 
     submit () {
-        let modifiedData = [
+        if (this.password == '') {
+            this.errorMessage = 'Password를 입력하세요';
+            return;
+        }
+        if (!this.phone.match(/^010-\d{4}-\d{4}$/)) {
+            this.errorMessage = '잘못된 전화번호 입니다.';
+            return;
+        }
+        var modifiedData = [
             this.password,
+            this.phone,
             this.id
         ];
 
         this.http.post('http://localhost:5000/update-sign-up-data', modifiedData).
             subscribe(response => {
-                this.saveError = false;
+                this.errorMessage = '';
+                this.router.navigate(['/login']);
             }, error => {
-                this.saveError = true;
+                this.errorMessage = '저장하지 못했습니다.';
                 console.log('my page component submit', error);
             });
     }
